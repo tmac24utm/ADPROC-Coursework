fd177ef2ee1bb083c807458cddc5deb755613651
Tom Macklam <tom_macklam@hotmail.com>
2016-11-17 14:34:54 +0000

    Updated Main Class
    Started on the input.

--- FlexBox/src/flexbox/FlexBox.java
+++ FlexBox/src/flexbox/FlexBox.java
@@ -10,13 +10,26 @@ package flexbox;
  * @author tom_m
  * @author danni
  */
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class FlexBox {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        int boxType = 0;
+        
+        try{
+            System.out.println("Please enter the type of box you need: ");
+            boxType = input.nextInt();
+        }
+        catch (Exception e){
+            System.out.println("Error");
+        }
     }
     
 }
