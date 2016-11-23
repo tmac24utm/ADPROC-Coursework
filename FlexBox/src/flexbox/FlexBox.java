2f0dc15e82ef18ceee325c8f7c48a813dc4f6cdb
Tom Macklam <tom_macklam@hotmail.com>
2016-11-23 19:21:54 +0000

    Added more stuff that doesnt work
    why

--- FlexBox/src/flexbox/FlexBox.java
+++ FlexBox/src/flexbox/FlexBox.java
@@ -10,12 +10,13 @@ public class FlexBox {
         Scanner input = new Scanner(System.in);
         int cardGrade = 0;
         int colourPrint = 0;
+        boolean bottomReinforcements = false;
         int errorCount = 1; //Is 1 so when it prints how many tries the user has used, it is easier to understand.
 
-        while (errorCount != 6) { //Is 6 because still gives a limit of 5, but makes it easier for the user to understand.
+        while (errorCount != 6) {
             try {
                 while (true) {
-                    System.out.println("Please enter the card grade: ");
+                    System.out.println("Please enter the card grade(Between 1-5 Inclusive): ");
                     int userInput = input.nextInt();
                     if (userInput >= 1 && userInput <= 5) {
                         System.out.println("You requested card grade " + userInput);
@@ -31,30 +32,30 @@ public class FlexBox {
                 }
 
                 while (true) {
-                    System.out.println("Would you like colour print? (Please enter, 0, 1 or 2): ");
+                    System.out.println("Would you like colour print? (Please enter, 0, 1 or 2)");
                     int userInput2 = input.nextInt();
-                    if (userInput2 >= 0 && userInput2 <= 2) {
-                        switch (userInput2) {
-                            case 0:
-                                System.out.println("You requested no colour.");
-                                colourPrint = userInput2;
-                                break;
-                            case 1:
-                                System.out.println("You requested 1 colour");
-                                colourPrint = userInput2;
-                                break;
-                            case 2:
-                                System.out.println("You requested 2 colours");
-                                colourPrint = userInput2;
-                                break;
-                        }
-                    } else {
-                        System.out.println("Error! Please enter an integer between 0-2(Inclusive). You have used " + errorCount + "/5 tries.");
-                        errorCount++;
-                        if (errorCount > 5) {
+
+                    switch (userInput2) {
+                        case 0:
+                            System.out.println("You requested no colour.");
+                            colourPrint = userInput2;
                             break;
-                        }
+                        case 1:
+                            System.out.println("You requested 1 colour");
+                            colourPrint = userInput2;
+                            break;
+                        case 2:
+                            System.out.println("You requested 2 colours");
+                            colourPrint = userInput2;
+                            break;
+                        default:
+                            System.out.println("Error! Please enter an integer between 0-2(Inclusive). You have used " + errorCount + "/5 tries.");
+                            errorCount++;
+                            if (errorCount > 5) {
+                                break;
+                            }
                     }
+                    break;
                 }
 
             } catch (InputMismatchException e) {
@@ -62,6 +63,27 @@ public class FlexBox {
                 errorCount++;
                 input.next();
             }
+
+            while (true) {
+                System.out.println("Would you like bottom reinforcement? (Please enter Yes or No)");
+                String userInput3 = input.next();
+                switch (userInput3) {
+                    case "Yes":
+                        System.out.println("You have requested bottom reinforcements.");
+                        bottomReinforcements = true;
+                        System.exit(0);
+                    case "No":
+                        System.out.println("You have requested no bottom reinforcements.");
+                        System.exit(0);
+                    default:
+                        System.out.println("Please enter a valid input. You have used " + errorCount + "/5 tries.");
+                        errorCount++;
+                        if (errorCount > 5) {
+                            System.exit(0);
+                        }
+                }
+            }
+
         }
     }
 }
